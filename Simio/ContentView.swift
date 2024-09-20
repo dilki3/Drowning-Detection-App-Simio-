213253a38dd6b2f7af53ad79a4f1570d2511109c
dilki3 <dilkidinesha345@gmail.com>
2024-09-21 00:07:42 +0530

    Initial Commit

---
+++ Simio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simio
+//
+//  Created by Dila Dinesha on 2024-09-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simio/SimioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimioApp.swift
+//  Simio
+//
+//  Created by Dila Dinesha on 2024-09-21.
+//
+
+import SwiftUI
+
+@main
+struct SimioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
